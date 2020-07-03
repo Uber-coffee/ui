103508b02390790bd33aff9682043234409c37ad
Maxim Bashilov <maxim.bashilov@yandex.ru>
2020-07-03 17:04:59 +0300

    feature for login

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header/Header';
-import Content from './components/Content/Content';
 import Footer from './components/Footer/Footer';
 import {Route} from 'react-router-dom';
-import Login from './components/Login/Login';
-function App () {
+import LoginContainer from './components/Login/LoginContener/LoginContainer';
+import ContentContainer from './components/Content/ContentContainer';
+function App (props) {
   return (
     <div className="AppWrapper">
       <div className="Header">
         <Header />
       </div>
       <div className="Conten">
-        <Content />
+        <ContentContainer store={props.store} />
       </div>
       <div className="Footer">
         <Footer />
       </div>
-      <Route path="/login" render={() => <Login />} />
+      <Route
+        path="/login"
+        exact
+        render={() => <LoginContainer store={props.store} />}
+      />
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,12 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import store from './redux/store';
 import {BrowserRouter as Router} from 'react-router-dom';
+import {Provider} from 'react-redux';
 
 ReactDOM.render (
   <React.StrictMode>
     <Router>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </Router>
   </React.StrictMode>,
   document.getElementById ('root')
