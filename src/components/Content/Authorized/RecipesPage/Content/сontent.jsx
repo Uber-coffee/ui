6f82805dae24c4a6177a46eb975176b9fab26651
SelyankinFyodor <fedkahuligan@gmail.com>
2020-07-17 09:49:03 +0300

    fix getting recipe data

--- src/components/Content/Authorized/RecipesPage/Content/сontent.jsx
+++ src/components/Content/Authorized/RecipesPage/Content/сontent.jsx
@@ -7,23 +7,6 @@ import RecipesTable from "./RecipesTable/recipes-table";
 import * as axios from "axios";
 
 const Content = () => {
-    const [dumbRecipes] = useState(
-        [
-            {
-                NAME: "ESPRESSO", COMPONENTS: [
-                    {NAME: "CUP", QUANTITY: 1, MEASURE: "PIECE"},
-                    {NAME: "COFFEE", QUANTITY: 20, MEASURE: "GR"}
-                ]
-            },
-            {
-                NAME: "CAPPUCCINO", COMPONENTS: [
-                    {NAME: "CUP", QUANTITY: 1, MEASURE: "PIECE"},
-                    {NAME: "COFFEE", QUANTITY: 20, MEASURE: "GR"},
-                    {NAME: "MILK", QUANTITY: 200, MEASURE: "ML"}
-                ]
-            }
-        ]
-    );
     const [realRecipes, setRealRecipes] = useState([]);
 
     //getting beverages & components from data base to create recipes table
@@ -35,19 +18,7 @@ const Content = () => {
                 }
             })
             .then(beverageResponse => {
-                axios
-                    .get('http://ecse005008ef.epam.com:8080/api/menu-service/w/components', {
-                        headers: {
-                            authorization: localStorage.getItem('jwt-Token')
-                        }
-                    })
-                    .then(componentsResponse => {
-                        createRecipesData(beverageResponse, componentsResponse);
-                    })
-                    .catch(error => {
-                        alert("getting components error!");
-                        console.log(error);
-                    });
+                createRecipesData(beverageResponse);
             })
             .catch(error => {
                 alert("getting beverages error!");
@@ -55,25 +26,19 @@ const Content = () => {
             });
     }, []);
 
-    const createRecipesData = (beverageResponse, componentsResponse) => {
-        const recipesArray = []
+    const createRecipesData = (beverageResponse) => {
+        const recipesArray = [];
         beverageResponse.data.map(element => {
             let newElem = {
-                NAME: element.name,
-                COMPONENTS: []
-            };
-            element.recipe.map(recipeEl => {
-                componentsResponse.data.map(compEl => {
-                    if (recipeEl.componentId === compEl.id) {
-                        let newCompEl = {
-                            NAME: compEl.name,
-                            QUANTITY: recipeEl.quantity,
-                            MEASURE: compEl.measure
-                        };
-                        newElem.COMPONENTS.push(newCompEl);
+                NAME: element.beverageName,
+                COMPONENTS: element.recipe.map(component => {
+                    return {
+                        NAME: component.component.name,
+                        MEASURE: component.component.measure,
+                        QUANTITY: component.quantity
                     }
-                });
-            });
+                })
+            };
             recipesArray.push(newElem);
         });
         setRealRecipes(recipesArray);
